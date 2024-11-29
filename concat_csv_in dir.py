f434157411c4f7da6931c56a3e7ac36532c889f5
jimbou <jimbou1999@gmail.com>
2024-11-29 10:05:53 +0800

    did postprocessing to the data of apps 4 mini 5 reruns

--- concat_csv_in dir.py
+++ concat_csv_in dir.py
@@ -28,8 +28,8 @@ def find_and_concat_csvs(directory, output_file):
         print("No CSV files found.")
 
 # Directory to search for CSV files
-input_directory = "/home/jim/HoarePrompt-data/Results/Pilot_confidence_simple/apps_3point5_3"  # Replace with your directory path
-output_csv = "/home/jim/HoarePrompt-data/Results/Pilot_confidence_simple/apps_3point5_3/combined_confidence_apps_3point5_3.csv"         # Replace with your desired output file name
+input_directory = "/home/jim/HoarePrompt-data/Results/Pilot_new2/apps_4_mini_5"  # Replace with your directory path
+output_csv = "/home/jim/HoarePrompt-data/Results/Pilot_new2/apps_4_mini_5/combined_apps_4_mini_5.csv"         # Replace with your desired output file name
 
 # Call the function
 find_and_concat_csvs(input_directory, output_csv)
